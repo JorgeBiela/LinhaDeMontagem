834ca99a8bc0fc2a745c53426492eb7d292b65ad
biela <jorge@gmail.com>
2021-03-16 19:33:19 -0400

    PARSE BIT FOR GIT

---
+++ src/main/java/mainclass/MainClass.java
@@ -0,0 +1,14 @@
+package mainclass;
+
+import modulo.linhademontagem.LinhaDeMontagem;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		LinhaDeMontagem linhaDeMontagem = new LinhaDeMontagem();
+		linhaDeMontagem.execute();
+
+	}
+
+}
